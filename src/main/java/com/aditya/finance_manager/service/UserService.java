8517f04cfd440b22d16f45db1ab770be6ebc0087
Aditya Raj <adityarajchaudhary12@gmail.com>
2026-09-15 22:02:53 +0530

    Added spring security authorization

--- src/main/java/com/aditya/finance_manager/service/UserService.java
+++ src/main/java/com/aditya/finance_manager/service/UserService.java
@@ -6,6 +6,7 @@ import com.aditya.finance_manager.entity.User;
 import com.aditya.finance_manager.exception.UserAlreadyExistsException;
 import com.aditya.finance_manager.mapper.UserMapper;
 import com.aditya.finance_manager.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,13 +14,15 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public UserService(
             UserRepository userRepository,
-            UserMapper userMapper
+            UserMapper userMapper, PasswordEncoder passwordEncoder
     ) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserResponse createUser(CreateUserRequest request) {
@@ -29,6 +32,8 @@ public class UserService {
         }
 
         User user = userMapper.toEntity(request);
+
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
 
         User savedUser = userRepository.save(user);
 
